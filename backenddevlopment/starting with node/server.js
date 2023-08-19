289a64a0cc1222a5e8bdb98cdd04520d02eb0870
OnkarDivekar07 <onkardivekar07@gmail.com>
2023-08-19 16:08:10 +0530

    made form which write to text file and redirect to 302

--- backenddevlopment/starting with node/server.js
+++ backenddevlopment/starting with node/server.js
@@ -1,33 +1,62 @@
 const http = require('http');
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-       console.log(req.url, req.method, req.headers)
+       //console.log(req.url, req.method, req.headers)
        // process.exit();
        const url = req.url;
-       if (url === '/home') {
-              res.setHeader('Content-header', 'text/html');
+       const method = req.method;
+       if (url === '/') {
+              res.write('<html>');
+              res.write('<head><title>this is my form Page</title></head>');
+              res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+              res.write('</html>');
+              return res.end();
+       }
+        if (url === '/message' && method === "POST") {
+                 const body = []
+                 req.on('data', (chunk) => {
+                      body.push(chunk);
+                    console.log(chunk);
+             })
+                  return    req.on('end', () => {
+                     const parsebody = Buffer.concat(body).toString();
+                     const message = parsebody.split('=')[1];
+                     fs.writeFile('message.txt', message,(err)=>{
+                           res.statusCode = 302;
+                           res.setHeader('Location', '/');
+                          return res.end();
+                    })
+                     
+               })
+
+        }
+
+       //response for /home request
+       else if (url === '/home') {
               res.write('<html>');
               res.write('<head><title>this is my home Page</title></head>');
               res.write('<body><h1>Welcome Home</h1></body>');
               res.write('</html>');
               res.end();
        }
+       //response for /about request
        else if (url === '/about') {
-              res.setHeader('Content-header', 'text/html');
               res.write('<html>');
               res.write('<head><title>this is my about us Page</title></head>');
               res.write('<body><h1>Welcome to About Us page</h1></body>');
               res.write('</html>');
               res.end();
        }
+       //response for /node request
        else if (url === '/node') {
-              res.setHeader('Content-header', 'text/html');
               res.write('<html>');
               res.write('<head><title>this is my project Page</title></head>');
               res.write('<body><h1>Welcome to my Node Js project</h1></body>');
               res.write('</html>');
               res.end();
        }
-       res.setHeader('Content-header', 'text/html');
+       // response on localhost:4000;
+       res.setHeader('Content-Type', 'text/html');
        res.write('<html>');
        res.write('<head><title>this is my first Page</title></head>');
        res.write('<body><h1>welcome to my node.js server page</h1></body>');
