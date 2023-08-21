1c3fa9cb610dfbaaee7c176ba6015bd2c40111f4
OnkarDivekar07 <onkardivekar07@gmail.com>
2023-08-21 23:29:19 +0530

    form is created on about-product route and product name and size added to it

--- backenddevlopment/starting with node/server.js
+++ backenddevlopment/starting with node/server.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const app = express();
-app.use((req, res, next) => {
-    console.log('this is middleware')
+const bodyParser= require('body-parser')
 
-    next();//allows to continue request to the next middleware
+app.use(bodyParser.urlencoded({extended:  false}));
 
+app.use('/add-product', (req, res, next) => {
+
+    res.send('<html><body><form action="/product" method="post"><input type="text" name="title" /><br><input type="number" name="size" /><br><button type="submit">Add product</button></form></body></html>')
 
 })
 
-app.use((req, res, next) => {
-    console.log('this is another middleware')
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+
+app.use('/',(req, res, next) => {
 
     res.send('<h1>Hello from express js</h1>')
 
