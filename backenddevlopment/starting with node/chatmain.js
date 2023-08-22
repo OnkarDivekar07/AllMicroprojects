45f71391328f34c43a2e61a0d8b4b53c249c8aaf
OnkarDivekar07 <onkardivekar07@gmail.com>
2023-08-22 23:28:00 +0530

    completed chat app lication task

--- backenddevlopment/starting with node/chatmain.js
+++ backenddevlopment/starting with node/chatmain.js
@@ -1,50 +1,47 @@
 const express = require('express')
-const fs=require('fs');
+const fs = require('fs');
 
 const chatapp = express();
 
-const bodyParse=require('body-parser');
+const bodyParse = require('body-parser');
 const { error } = require('console');
 
-chatapp.use(bodyParse.urlencoded({extended: false}))
+chatapp.use(bodyParse.urlencoded({ extended: false }))
 
 
-chatapp.use("/login", (req, res, next) => {
-    fs.readFile('index.html', 'utf-8',(error,htmlContent)=>{
-       if(error){
-        res.status(400).send('failed to load html content')
-       }
-       else{
-        res.send(htmlContent);
-       }
-
-
-    })
-    
+chatapp.get("/login", (req, res, next) => {
+    res.send('<form onsubmit="localStorage.setItem(`username`,document.getElementById(`username`).value)" action="/user" method="POST"><input id="username" type="text" name="username"><br><button type="submit">Login</button></form>')
 })
 
+chatapp.post("/user", (req, res, next) => {
+    console.log(req.body);
+    const a = req.body.username
+    if (req.body.chat && req.body.username) {
+        fs.writeFile('chat.txt', `${req.body.username} : ${req.body.chat}  `, { flag: 'a' }, (error) => {
+            if (error) {
+                console.log(error)
+
+            }
+            else {
+                console.log('succesfully created');
+            }
+        })
+    }
+    fs.readFile('chat.txt', (err, data) => {
+        if (err) {
+            data = "cannot find any chat"
+        }
+        res.send(`${data}<form action="/user" method="POST" onsubmit="document.getElementById('username').value=localStorage.getItem('username')">
+        <input type="text" id="chat" name="chat" placeholder="message">
+        <input type="hidden" id="username" name="username">
+        <button type="submit">SEND</button>
+        </form>`);
+    });
+
+})
 
 chatapp.use("/", (req, res, next) => {
-    if(req.body.chat){
-        fs.appendFile('chat.txt',req.body.chat,(error)=>{
-      if(error){
-        console.log(error)
-      }
-      else{
-        console.log('succesfully created');
-      }
-})
-    }
+    res.redirect('/login')
 
-    fs.readFile('chat.txt','utf-8',(error,chats)=>{
-        if(error){
-            console.log(error)
-        }
-        else{
-            res.send(`<html><body><form  action="/chat" method="Post"><p>${chats}</p><input type="text" name="chat" placeholder="write here" /><br><button type="submit">send</button></form></body></html>`)  
-        }
-    })
-
-    
 })
 chatapp.listen(4000);
