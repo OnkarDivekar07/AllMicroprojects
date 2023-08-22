0862589e05a9fa731eaa938eda1ea27b75d5451c
OnkarDivekar07 <onkardivekar07@gmail.com>
2023-08-22 14:02:12 +0530

    the localstorage facility added for saving the user input after login and redirecting user from login page to the chat page

--- backenddevlopment/starting with node/chatmain.js
+++ backenddevlopment/starting with node/chatmain.js
@@ -1,21 +1,30 @@
 const express = require('express')
+const fs=require('fs');
 
 const chatapp = express();
 
+const bodyParse=require('body-parser');
+const { error } = require('console');
+
+chatapp.use(bodyParse.urlencoded({extended: false}))
+
 
 chatapp.use("/login", (req, res, next) => {
+    fs.readFile('index.html', 'utf-8',(error,htmlContent)=>{
+       if(error){
+        res.status(400).send('failed to load html content')
+       }
+       else{
+        res.send(htmlContent);
+       }
 
-    res.send('<html><body><form  action="/user" method="post"><input type="text" name="chat" placeholder="username" /><br><button type="submit">Login</button></form></body></html>')
+
+    })
     
 })
 chatapp.use("/", (req, res, next) => {
 
     res.send('<html><body><form  action="/chat" method="Post"><input type="text" name="chat" placeholder="write here" /><br><button type="submit">send</button></form></body></html>')
 })
-
-
-
-
-
 
 chatapp.listen(4000);
